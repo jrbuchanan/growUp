75c6b144f862d3d84d3bb89f79905a992ac0fc3e
jrbuchanan <joe@metrofbc.com>
2019-03-30 16:22:39 -0500

    Up to Week 5

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
+//*need to add a this statement to jquery to make work 
+$(document).ready ( () =>{
+    $('.week-odd').on('click', event =>{
+        $(event.target).next(".week-content").slideToggle(500);
+    });
+    $('.week-even').on('click', event => {
+        $(event.target).next(".week-content").slideToggle(500);
+    });
+
+});
+
 function goBack() {
     window.history.back();
 }
+
+
