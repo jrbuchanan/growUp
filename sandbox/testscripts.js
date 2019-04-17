491dc9a8da7a53989717659b58e4908a27773274
jrbuchanan <joe@metrofbc.com>
2019-04-17 12:17:59 -0500

    changed link to esv.org rather than bibia

--- sandbox/testscripts.js
+++ sandbox/testscripts.js
@@ -1,5 +1,3 @@
-
-
 var checkboxValues = JSON.parse(localStorage.getItem('checkboxValues')) || {};
 
 var $checkboxes = $(".checkbox-container :checkbox");
@@ -12,13 +10,13 @@ $(document).ready(() => {
     $('.week-even').on('click', event => {
         $(event.target).next(".checkbox-container").slideToggle(1000);
     });
-  
+
     //*iterate over checkboxes and set the values accordingly
-     $(".checkbox-container :checkbox").on("change", function () {
-         
-         checkboxValues[this.id] = this.checked;
+    $(".checkbox-container :checkbox").on("change", function () {
+
+        checkboxValues[this.id] = this.checked;
         localStorage.setItem('checkboxValues', JSON.stringify(checkboxValues));
-     });
+    });
 
     $.each(checkboxValues, function (key, value) {
         $("#" + key).prop('checked', value);
@@ -26,23 +24,16 @@ $(document).ready(() => {
 
     //*Event Listener for Scripture passages
 
-    $('.scripture').on('click', event =>{
-        let passage=$(event.target).text();
-        // console.log(passage);
-        // window.open("https://biblia.com/bible/esv/"+passage);
+    $('.scripture').on('click', event => {
+        let passage = $(event.target).text();
 
-        
-        // let width=window.innerWidth/2;
-        // let height=window.innerHeight/2;
-        // console.log(width,height);
+        /*set width and height relative to screen size*/
+        let features= "width="+window.outerWidth/2+ ', height='+ window.outerHeight/2 ;
 
+        window.open("https://esv.org/" + passage,'scripture-window',features);
 
-        let features = "width=" + window.innerWidth / 2 + ', height=' + window.innerHeight / 2+', top=0, left=0';
+    });
 
-        window.open("https://esv.org/" + passage, '_blank');
-             
-    })
-    
 });
 
 function goBack() {
